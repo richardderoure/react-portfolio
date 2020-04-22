e2b0ede03326c52c7d28d4bf708186e7a9b930ac
huseyin <thehusko@gmail.com>
2020-04-22 16:52:15 +0100

    MVP, need to possibly configure some styling and add to About Me

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { Card, CardBody, Button, CardTitle, CardText, CardImg } from 'reactstrap';
+import { Card, Button, CardImg, CardTitle, CardText, CardDeck, CardSubtitle, CardBody } from 'reactstrap';
 
-const Portfolio = (props) => {
+class Portfolio extends React.Component {
+  state = {
+    data: null,
+  }
+
+  async componentDidMount(){
+    const url = "https://api.github.com/users/richardderoure/repos"
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({data: data});
+  }
+  
+  
+render(){
+  
+//add max text limit for description
+const getImg = () => {
+  return "https://source.unsplash.com/random/500x500"
+}
+
   return (
-    <div className="card1">
-      <Card>
-        <CardImg top width="100%" src="https://images.unsplash.com/photo-1522199794616-8a62b541f762?ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card Title</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-        </CardBody>
-        <Button>Go somewhere</Button>
-      </Card>
-    </div>
+    <div className="card-container">
+      {!this.state.data ? "loading data..." :
+      this.state.data.map( project => 
+          <Card className="card1">
+            <CardImg top width="100%" src={getImg()} alt="Card image alt" />
+            <CardBody>
+              <CardTitle>{project.name}</CardTitle>
+              <CardText>{!project.description ? 'description' : project.description}</CardText>
+              <a href={project.html_url}><Button>Go to GitHub</Button></a>
+            </CardBody>
+          </Card>
+      )}
+      </div>
   );
 };
+}
 
 export default Portfolio;
